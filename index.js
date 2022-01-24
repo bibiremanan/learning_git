9523df46b4f18b71814a5c795612f6d99ff4f296
bibiremanan <bibiremanan0101@gmail.com>
2022-01-24 20:08:38 +0530

    add subtraction

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const addition = (number1, number2) => {
   return number1 + number2;
 };
 
-export { addition };
+const subtraction = (number1, number2) => {
+  return number1 - number2;
+};
+
+export { addition, subtraction };
